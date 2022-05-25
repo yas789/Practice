69e42422f7c640803f1f39770f6e5839f7141354
yassir maknaoui <yassirmaknaoui@yassirs-mbp.home>
2022-05-25 19:09:23 +0100

    Initial commit

---
+++ Fiel.java
@@ -0,0 +1,5 @@
+public Fiel {
+    public static void main(String [] args){
+        System.out.println("hi")
+    }
+}
---
+++ Interview.java
@@ -0,0 +1,5 @@
+class Interview {
+    public static void main(String [] args ){
+        System.out.println("hello world");
+    }
+}
